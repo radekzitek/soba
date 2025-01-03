a95ecf835a143ab9d7a45cc5d725ae507a02e051
Radek Zítek <radekzitek@gmail.com>
2025-01-03 09:30:30 +0100

    Remove App.vue, main.ts, SystemDebug.vue, vuetify.ts, and system.ts files to streamline the project structure and eliminate unused components. This cleanup enhances maintainability and prepares the codebase for future development.

--- src/plugins/vuetify.ts
+++
@@ -1,9 +0,0 @@
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import '@mdi/font/css/materialdesignicons.css'
-
-export default createVuetify({
-  theme: {
-    defaultTheme: 'light'
-  }
-})
